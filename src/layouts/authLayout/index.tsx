0972e39cefc7574636aac427404237603bf777cc
Mikael Marceniuk <mikael_marceniuk@protonmail.com>
2022-02-08 21:13:58 -0300

    Developed AuthLayout

--- src/layouts/authLayout/index.tsx
+++ src/layouts/authLayout/index.tsx
@@ -1,11 +1,22 @@
+import { Col, Container, Row } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
+import { BsFillHandThumbsUpFill } from 'react-icons/bs'
+import './index.css'
 
 const AuthLayout = () => {
 	return (
-		<div>
-			Auth Layout
-			<Outlet />
-		</div>
+		<Container className="auth_container" fluid>
+			<Row>
+				<Col className="auth_content">
+					<BsFillHandThumbsUpFill />
+					<h1>Segware Code Challenge</h1>
+					<p>Upvotes app</p>
+				</Col>
+				<Col md={7} lg={8}>
+					<Outlet />
+				</Col>
+			</Row>
+		</Container>
 	)
 }
 
