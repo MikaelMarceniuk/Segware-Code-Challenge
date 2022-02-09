ed1e557cc43989e7b3332e3c32eebad545ac5deb
Mikael Marceniuk <mikael_marceniuk@protonmail.com>
2022-02-08 21:24:00 -0300

    Designed SignIn Page;

--- src/layouts/authLayout/index.tsx
+++ src/layouts/authLayout/index.tsx
@@ -1,18 +1,18 @@
 import { Col, Container, Row } from 'react-bootstrap'
 import { Outlet } from 'react-router-dom'
 import { BsFillHandThumbsUpFill } from 'react-icons/bs'
-import './index.css'
+import './style.css'
 
 const AuthLayout = () => {
 	return (
-		<Container className="auth_container" fluid>
+		<Container fluid>
 			<Row>
 				<Col className="auth_content">
 					<BsFillHandThumbsUpFill />
 					<h1>Segware Code Challenge</h1>
 					<p>Upvotes app</p>
 				</Col>
-				<Col md={7} lg={8}>
+				<Col md={7} lg={8} className="auth_outlet">
 					<Outlet />
 				</Col>
 			</Row>
--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,5 +1,30 @@
+import { Button, Container, Form } from 'react-bootstrap'
+import './style.css'
+
 const SignInPage = () => {
-	return <h3>SignIn</h3>
+	return (
+		<Container>
+			<h1 className="signin_title">Sign in</h1>
+			{/* Email */}
+			<Form.Group className="mb-3" controlId="formBasicEmail">
+				<Form.Label>Email address</Form.Label>
+				<Form.Control type="email" />
+				<Form.Text className="text-muted">
+					We will never share your email with anyone else.
+				</Form.Text>
+			</Form.Group>
+
+			{/* Password */}
+			<Form.Group className="mb-3" controlId="formBasicPassword">
+				<Form.Label>Password</Form.Label>
+				<Form.Control type="password" />
+			</Form.Group>
+
+			<div className="d-grid">
+				<Button variant="primary">Sign in</Button>
+			</div>
+		</Container>
+	)
 }
 
 export default SignInPage
